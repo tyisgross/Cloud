1fdc9623bb14db0fe39221caf1049a4f71ee5321
tyisgross <158226333+tyisgross@users.noreply.github.com>
2026-06-10 20:31:29 -0700

    readme update

--- Flask/Readme.md
+++ Flask/Readme.md
@@ -1,4 +1,4 @@
-This Flasck project was a proof of concept and learning opportunity in administering cloud applications via docker and terraform. This was the first time I have pulled an application from github and pushed it to my docker hub. This was also my first project building infrastructure in AWS via terraform. I was able to generate all of the necessary networking to connect to the instance I created via SSH but was unable to get the flask app to correctly run. Here was the error I received when looking at the logs of the Flask application: 
+This Flasck project was a proof of concept and learning opportunity in administering cloud applications via docker and terraform. This was the first time I have pulled an application from github and pushed it to my docker hub. This was also my first project building infrastructure in AWS via terraform. I was able to generate all of the necessary networking and security configurations to connect to the instance I created via SSH, but was unable to get the flask app to correctly run. Here was the error I received when looking at the logs of the Flask application: 
 
 "Traceback (most recent call last):
   File "/app/app.py", line 1, in <module>
@@ -7,4 +7,4 @@ ModuleNotFoundError: No module named 'flask'
 ubuntu@ip-10-0-1-128:~$"
 
 
-I have decided to move onto a new project that will use Kubernetes and the respective AWS services utilizaing Kubernetes.
+I have decided to move onto a new project that will use Kubernetes and the respective AWS services utilizing Kubernetes.
--- Flask/main.tf
+++ Flask/main.tf
@@ -2,6 +2,77 @@ provider "aws" {
   region = "us-west-2"
 }
 
+resource "aws_vpc" "network" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  
+  tags = {
+    Name = "my-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.network.id
+
+  tags = {
+    Name = "main-gateway"
+  }
+}
+
+resource "aws_subnet" "subnet" {
+  vpc_id = aws_vpc.network.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "my-subnet"
+  }
+}
+
+resource "aws_route_table" "route" {
+  vpc_id = aws_vpc.network.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.subnet.id
+  route_table_id = aws_route_table.route.id
+}
+
+resource "aws_security_group" "web" {
+  name   = "web-sg"
+  vpc_id = aws_vpc.network.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["my public ip/32"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["my public ip/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -13,31 +84,33 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "app_server" {
+resource "aws_instance" "flask_server" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
+
+  subnet_id = aws_subnet.subnet.id
+
+  vpc_security_group_ids = [aws_security_group.web.id]
+
+  associate_public_ip_address = true
+
+  key_name = "MyKeyPair"
 
   tags = {
-    Name = "learn-terraform"
+    Name = "flask-app"
   }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt update
+              apt install docker.io -y
+              systemctl start docker
+              systemctl enable docker
+              docker pull tyanthonygross/cloud-apps:flask
+              docker run -d -p 80:8000 tyanthonygross/cloud-apps:flask
+              EOF
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "app_server" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "learn-terraform"
-  }
+output "public_ip" {
+  value = aws_instance.flask_server.public_ip
 }
