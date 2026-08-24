232e7aaf8d2720fe6164fc2518a7256f30553d2c
tyisgross <158226333+tyisgross@users.noreply.github.com>
2026-08-23 22:39:06 -0700

    0823206

--- Kubernetes-Stack-1/Modules/Kubernetes/main.tf
+++ Kubernetes-Stack-1/Modules/Kubernetes/main.tf
@@ -4,16 +4,83 @@ provider "aws" {
 
 terraform {
   required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-
     kubernetes = {
-      source = "hashicorp/kubernetes"
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
     }
 
     helm = {
-      source = "hashicorp/helm"
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
     }
   }
 }
+
+provider "kubernetes" {
+  host                   = var.cluster_endpoint
+  cluster_ca_certificate = base64decode(var.cluster_ca)
+  token                  = var.cluster_token
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = var.cluster_endpoint
+    cluster_ca_certificate = base64decode(var.cluster_ca)
+    token                  = var.cluster_token
+  }
+}
+
+resource "kubernetes_deployment_v1" "nginx" {
+  metadata {
+    name = "nginx"
+  }
+
+  spec {
+    replicas = 2
+
+    selector {
+      match_labels = {
+        app = "nginx"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "nginx"
+        }
+      }
+
+      spec {
+        container {
+          name  = "nginx"
+          image = "nginx:latest"
+
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service_v1" "nginx" {
+  metadata {
+    name = "nginx"
+  }
+
+  spec {
+    selector = {
+      app = "nginx"
+    }
+
+    port {
+      port        = 80
+      target_port = 80
+    }
+
+    type = "LoadBalancer"
+  }
+}
+
