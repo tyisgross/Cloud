b999444c82783dd896ba24470b464b0ed1b997ea
tyisgross <158226333+tyisgross@users.noreply.github.com>
2026-09-14 21:04:21 -0700

    09142026-1

--- Kubernetes-Stack-1/providers.tf
+++ Kubernetes-Stack-1/providers.tf
@@ -2,17 +2,22 @@ terraform {
   required_providers {
       aws = {
         source  = "hashicorp/aws"
-        version = "~> 6.38"
-      }
-      
-      kubernetes = {
-        source  = "hashicorp/kubernetes"
+        version = ">= 5.95.0, < 6.0.0"
+        }
+
+      kubernetes = { 
+        source = "hashicorp/kubernetes"
         version = "~> 2.38"
-      }
+        }
 
       helm = {
         source  = "hashicorp/helm"
         version = "~> 3.2"
+        }
+
+      kubectl = {
+        source = "hashicorp-oss/kubectl"
+        version = "~> 0.1.13"
       }
     }
-}
+  }
